2af70c1036cc4509cb409cd683965235e0bb76f1
dataAiOliver <data.ai.oliver@gmail.com>
2024-05-02 22:14:12 +0200

    cleanup

--- sqlAgent/dummy.py
+++
@@ -1,8 +0,0 @@
-from langchain_community.agent_toolkits import create_sql_agent
-from langchain_openai import ChatOpenAI
-from langchain_openai import AzureChatOpenAI
-from langchain_community.utilities import SQLDatabase
-import logging, os
-from langchain_openai import OpenAI
-from sqlalchemy import create_engine
-import pandas as pd
