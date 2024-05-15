7a75d4b514d194695f1eec8a86dec017f9e499c3
dataAiOliver <data.ai.oliver@gmail.com>
2024-05-15 11:30:55 +0200

    embeddings

---
+++ embeddings/dummy.py
@@ -0,0 +1,21 @@
+import gensim.downloader as api
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+import seaborn as sns
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+import pandas as pd
+import plotly.express as px
+import plotly, tqdm
+import xml.etree.ElementTree as ET
+import os, json, plotly, tqdm
+import numpy as np
+from sklearn.decomposition import PCA
+import plotly.express as px
+from sentence_transformers import SentenceTransformer
+import pandas as pd
+from sklearn.manifold import TSNE
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
